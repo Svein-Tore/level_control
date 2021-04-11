ef649f4f00fb579631f054e952b8c1da29ad6d37
Svein-Tore <svenar89@gmail.com>
2021-04-11 23:36:41 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,5 +2,6 @@ basic.forever(function () {
     serial.writeValue("Nivå.Er", 122 / 1000 * pins.analogReadPin(AnalogPin.P0) - 25)
     serial.writeValue("Nivå.low", 0)
     serial.writeValue("Nivå.high", 100)
+    serial.writeValue("Nivå.skal", 100 / 1023 * pins.analogReadPin(AnalogPin.P1))
     basic.pause(500)
 })
