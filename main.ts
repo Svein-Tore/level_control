cd8c6a0c2b1e31d47209e58180cde8e25072ab98
Svein-Tore <svenar89@gmail.com>
2021-09-02 13:40:03 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 basic.forever(function () {
-    serial.writeValue("Nivå.high", 100)
-    serial.writeValue("Nivå.Er", (0.1285 * pins.analogReadPin(AnalogPin.P0) - 24.7) / 1.023)
-    serial.writeValue("Nivå.skal", 100 * pins.analogReadPin(AnalogPin.P1) / 1023 / 1.023)
-    serial.writeValue("Nivå.low", 0)
-    basic.pause(500)
+    serial.writeValue("level.high", 100)
+    serial.writeValue("level.ER", (0.1285 * pins.analogReadPin(AnalogPin.P0) - 24.7) / 1.023)
+    serial.writeValue("level.skal", 100 * pins.analogReadPin(AnalogPin.P1) / 1023 / 1.023)
+    serial.writeValue("level.low", 0)
+    basic.pause(2000)
 })
